424dc11e656aa217daeecc068064de732cf77ff8
jormunrod <jorgemr@protonmail.ch>
2025-02-26 12:06:09 +0100

    feat: create caronte backend zero version

---
+++ src/main/java/com/caronte/caronte/CaronteApplication.java
@@ -0,0 +1,13 @@
+package com.caronte.caronte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaronteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaronteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caronte/caronte/CaronteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caronte.caronte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
