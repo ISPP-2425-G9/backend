c084e11ff063595119a79f066da69171a67863a8
davguifer <davguifer@gmail.com>
2025-03-01 17:00:30 +0100

    [fix] fix missing import

--- src/main/java/com/caronte/caronte/receiver/Receiver.java
+++ src/main/java/com/caronte/caronte/receiver/Receiver.java
@@ -11,6 +11,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
+import com.caronte.caronte.message.Message;
 
 @Entity
 @Getter
