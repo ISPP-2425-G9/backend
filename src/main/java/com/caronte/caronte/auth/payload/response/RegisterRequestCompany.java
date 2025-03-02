e6626ccc1b0f5aa15f6213e85a22fd4c3d696792
davguifer <davguifer@gmail.com>
2025-03-02 19:42:38 +0100

    style: change language of errors messages

--- src/main/java/com/caronte/caronte/auth/payload/response/RegisterRequestCompany.java
+++ src/main/java/com/caronte/caronte/auth/payload/response/RegisterRequestCompany.java
@@ -7,42 +7,81 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import com.caronte.caronte.company.Company;
+import com.caronte.caronte.plan.Plan;
+import com.caronte.caronte.plan.PlanType;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Setter
 public class RegisterRequestCompany {
 
-    @NotBlank(message = "Name is required")
+    @Length(max=100)
+    @NotBlank(message = "El nombre es requerido")
     private String name;
     
-    @NotBlank(message = "Email is required")
+    @Length(max=255)
+    @NotBlank(message = "El email es requerido")
     @Pattern(regexp = REGEX_EMAIL, message = "Invalid email format")
-    @JsonProperty("email")
-    private String id;
+    private String email;
+
+    @NotBlank(message = "Password1 es requerido")
+    private String password1;
+
+    @NotBlank(message = "Password2 es requerido")
+    private String password2;
     
-    @NotBlank(message = "Password is required")
-    private String password;
-    
-    @NotBlank(message = "Telephone is required")
+    @Length(max=15)
+    @NotBlank(message = "El teléfono es requerido")
     private String telephone;
     
-    @NotBlank(message = "Direction is required for company")
+    @Length(max=255)
+    @NotBlank(message = "La dirección es requerida")
     @JsonProperty("direction")
     private String address;
     
-    @NotBlank(message = "City is required for company")
+    @Length(max=100)
+    @NotBlank(message = "La ciudad es requerida")
     private String city;
     
-    
-    @NotBlank(message = "ZipCode is required for company")
+    @Length(max=10)
+    @NotBlank(message = "El código postal es requerido")
     private String zipCode;
     
-    @NotBlank(message = "NIF is required for company")
-    @Pattern(regexp = REGEX_NIF, message = "Invalid NIF format")
+    @Length(max=20)
+    @NotBlank(message = "El NIF de la compañia es requerido")
+    @Pattern(regexp = REGEX_NIF, message = "Formato de NIF invalido")
     private String nif;
     
+    @Length(max=512)
     @JsonProperty("image")
     private String imageUrl;
     
+    @Length(max=1024)
     private String description;
+
+    public Company parse(PasswordEncoder passwordEncoder){
+        Company company = new Company();
+        company.setAddress(this.getAddress());
+        company.setCity(this.getCity());
+        company.setZipCode(this.getZipCode());
+        company.setNif(this.getNif());
+        company.setDescription(this.getDescription());
+        company.setImageUrl(this.getImageUrl());
+        company.setName(this.getName());
+        company.setEmail(this.getEmail());
+        company.setPassword(passwordEncoder.encode(this.getPassword1()));
+        company.setTelephone(this.getTelephone());
+        
+        Plan plan = new Plan();
+        plan.setPlanType(PlanType.FREE);
+        plan.setExpireDate(null);
+        plan.setBillingAddress(null);
+        company.setPlan(plan);
+
+        return company;
+
+    }
 }
--- src/main/java/com/caronte/caronte/auth/payload/response/RegisterRequestCustomer.java
+++ src/main/java/com/caronte/caronte/auth/payload/response/RegisterRequestCustomer.java
@@ -1,35 +1,61 @@
 package com.caronte.caronte.auth.payload.response;
 
 import static com.caronte.caronte.util.RegexContants.REGEX_EMAIL;
+
+import java.time.LocalDate;
+
 import static com.caronte.caronte.util.RegexContants.REGEX_DNI;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import com.caronte.caronte.customer.Customer;
+import com.caronte.caronte.plan.Plan;
+import com.caronte.caronte.plan.PlanType;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Setter
 public class RegisterRequestCustomer {
 
-    @NotBlank(message = "Full name is required")
+    @NotBlank(message = "El nombre completo es requerido")
     @JsonProperty("full_name")
     private String name;
     
-    @NotBlank(message = "Email is required")
+    @NotBlank(message = "El email es requerido")
     @Pattern(regexp = REGEX_EMAIL, message = "Invalid email format")
     @JsonProperty("email")
     private String id;
     
-    @NotBlank(message = "Password is required")
-    private String password;
+    @NotBlank(message = "Password1 es requerida")
+    private String password1;
     
-    @NotBlank(message = "Telephone is required")
+    @NotBlank(message = "Password2 es requerida")
+    private String password2;
+    
+    @NotBlank(message = "El teléfono es requerido")
     private String telephone;
     
-    @NotBlank(message = "DNI is required for customer")
+    @NotBlank(message = "El DNI del usuario es requerido")
     @Pattern(regexp = REGEX_DNI, message = "Invalid DNI format")
     private String dni;
     
+    public Customer parse(PasswordEncoder passwordEncoder) {
+        Customer customer = new Customer();
+        customer.setDni(this.getDni());
+        customer.setIsActive(true);
+        customer.setName(this.getName());
+        customer.setEmail(this.getId());
+        customer.setPassword(passwordEncoder.encode(this.getPassword1()));
+        customer.setTelephone(this.getTelephone());
 
+        Plan plan = new Plan();
+        plan.setPlanType(PlanType.FREE);
+        plan.setExpireDate(null);
+        plan.setBillingAddress(null);
+        customer.setPlan(plan);
+        return customer;
+    }
 }
