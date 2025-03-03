ab542061f6f75df580323d6dbee608d266deff8c
alvarochico2408 <alvarochicocastellano@gmail.com>
2025-03-03 19:04:58 +0100

    feat: Cors Config class created

---
+++ src/main/java/com/caronte/caronte/configuration/CorsConfig.java
@@ -0,0 +1,17 @@
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica CORS a todos los endpoints
+                        .allowedOrigins("http://localhost:8081") // Permite el frontend en este origen
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos los headers
+                        .allowCredentials(true); // Permitir autenticación con cookies o headers
+            }
+        };
+    }
+}
