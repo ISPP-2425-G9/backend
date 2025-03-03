bc67f7ace7fa649aed1b894f526a4d2e22a60d58
alvarochico2408 <alvarochicocastellano@gmail.com>
2025-03-03 19:05:55 +0100

    fix: missing imports in corsConfig

--- src/main/java/com/caronte/caronte/configuration/CorsConfig.java
+++ src/main/java/com/caronte/caronte/configuration/CorsConfig.java
@@ -1,3 +1,10 @@
+package com.caronte.caronte.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
 
