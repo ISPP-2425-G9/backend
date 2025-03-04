b74bcdfafae97a236559f3e8babee90b3cb83ab1
jormunrod <jorgemr@protonmail.ch>
2025-03-04 19:44:02 +0100

    refactor: clean up pom.xml and optimize imports in AuthController and CustomerService

--- src/main/java/com/caronte/caronte/auth/AuthController.java
+++ src/main/java/com/caronte/caronte/auth/AuthController.java
@@ -3,8 +3,8 @@ package com.caronte.caronte.auth;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
--- src/main/java/com/caronte/caronte/customer/CustomerService.java
+++ src/main/java/com/caronte/caronte/customer/CustomerService.java
@@ -1,10 +1,6 @@
 package com.caronte.caronte.customer;
 
-import java.lang.classfile.ClassFile.Option;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
