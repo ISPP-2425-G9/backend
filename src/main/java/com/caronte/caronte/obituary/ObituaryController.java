cc31d8f716c25807c112b42d9eaf6a2706fe3f47
JavierRodriguez <javrodrei@alum.us.es>
2025-03-04 19:37:13 +0100

    feat: create Obituary with dto

--- src/main/java/com/caronte/caronte/obituary/ObituaryController.java
+++ src/main/java/com/caronte/caronte/obituary/ObituaryController.java
@@ -4,7 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.caronte.caronte.imageTemplate.ImageTemplateService;
+import com.caronte.caronte.obituary.ObituraryRequestDto.ContactDto;
 import com.caronte.caronte.receiver.ReceiverService;
+
+import jakarta.validation.Valid;
+
+import com.caronte.caronte.configuration.services.UserDetailsImpl;
 import com.caronte.caronte.customer.Customer;
 import com.caronte.caronte.customer.CustomerRepository;
 import com.caronte.caronte.imageTemplate.ImageTemplate;
@@ -13,6 +18,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("api/obituary")
@@ -31,41 +38,42 @@ public class ObituaryController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Obituary> createObituary(@RequestBody Map<String, Object> request) {
+    public ResponseEntity<Obituary> createObituary(@RequestBody @Valid ObituraryRequestDto request, Authentication authentication) {
         Obituary obituary = null;
         try{
-            String name = (String) request.get("name");
+            UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+            Long customerId = userPrincipal.getId();
+            String name = request.getName();
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            Date birthDate = dateFormat.parse((String)request.get("birthDate"));
-            Date deathDate = dateFormat.parse((String)request.get("deathDate"));
-            String customImageUrl = (String) request.get("customImage");
-            String farewellMessage = (String) request.get("farewellMessage");;
-            String farewellPhrase = (String) request.get("farewellPhrase");
-            Long customerId = Long.parseLong(request.get("customer_id").toString());
-            Long imageTemplateId = Long.parseLong(request.get("imageTemplate_id").toString());
+            Date birthDate = dateFormat.parse(request.getBirthDate());
+            Date deathDate = dateFormat.parse(request.getDeathDate());
+            String customImageUrl = request.getCustomImage();
+            String farewellMessage = request.getFarewellMessage();
+            String farewellPhrase = request.getFarewellPhrase();
+            Long imageTemplateId = Long.parseLong(request.getImageTemplate_id().toString());
             Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new RuntimeException("Customer not found"));
             ImageTemplate imageTemplate = imageTemplateService.findById(imageTemplateId);
-            Boolean is_mine = Boolean.parseBoolean(request.get("isMine").toString());
+            Boolean is_mine = request.getIsMine();
 
             obituary = obituaryService.saveObituary(
-                name,
-                birthDate,
-                deathDate,
-                customImageUrl,
-                farewellMessage,
-                farewellPhrase,
-                is_mine,
-                customer,
-                imageTemplate
-            );
+                 name,
+                 birthDate,
+                 deathDate,
+                 customImageUrl,
+                 farewellMessage,
+                 farewellPhrase,
+                 is_mine,
+                 customer,
+                 imageTemplate
+             );
             
-            List<Map<String, Object>> contacts = (List<Map<String, Object>>) request.get("contacts");
-            for (Map<String, Object> contact : contacts) {
-                String contactName = (String) contact.get("name");
-                String contactTelephone = (String) contact.get("phone");
-                String contactEmail = (String) contact.get("email");
-                receiverService.saveObituaryReceiver(contactName, contactTelephone, contactEmail, obituary);   
-            }  
+             List<ContactDto> contacts = request.getContacts();
+             for (ContactDto contact : contacts) {
+                 String contactName = contact.getName();
+                 String contactTelephone = contact.getPhone();
+                 String contactEmail = contact.getEmail();
+                 receiverService.saveObituaryReceiver(contactName, contactTelephone, contactEmail, obituary);   
+             }  
 
             
         }catch(Exception e){
---
+++ src/main/java/com/caronte/caronte/obituary/ObituraryRequestDto.java
@@ -0,0 +1,46 @@
+package com.caronte.caronte.obituary;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class ObituraryRequestDto {
+
+    private String name;
+
+    @NotNull(message = "La fecha de nacimiento no puede ser nula")
+    private String birthDate; 
+
+    private String deathDate;
+
+    private String customImage; 
+
+    private String farewellMessage;
+
+    private String farewellPhrase;
+
+    private Long imageTemplate_id;
+
+    @NotNull(message = "El campo 'isMine' no puede ser nulo")
+    private Boolean isMine;
+
+    private List<@Valid ContactDto> contacts;
+
+    @Getter
+    @Setter
+    public static class ContactDto {
+        @NotBlank(message = "El nombre del contacto no puede estar vacío")
+        private String name;
+
+        @Pattern(regexp = "\\d{9,15}", message = "El teléfono debe contener entre 9 y 15 dígitos numéricos")
+        private String phone;
+
+        @Email(message = "El email no es válido")
+        @NotBlank(message = "El email no puede estar vacío")
+        private String email;
+    }
+    
+}
