ffee8a7088d323c44710becb1cdece592becdc18
jormunrod <jorgemr@protonmail.ch>
2025-03-07 15:38:57 +0100

    feat: update Dockerfile to use new JAR version and modify CORS configuration

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:23-jdk
 COPY releases/caronte-0.0.1-SNAPSHOT.jar app.jar
 COPY releases/caronte-0.0.2-SNAPSHOT.jar app2.jar
-COPY releases/caronte-v1.jar v1.jar
+COPY releases/caronte-v1.1.jar v1.1.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "app2.jar"]
+ENTRYPOINT ["java", "-jar", "v1.1.jar"]
--- src/main/java/com/caronte/caronte/configuration/CorsConfig.java
+++ src/main/java/com/caronte/caronte/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica CORS a todos los endpoints
-                        .allowedOrigins("http://localhost:8081") // Permite el frontend en este origen
+                        .allowedOrigins("http://localhost:8081", "https://frontend-sprint1.up.railway.app") // Permite el frontend en este origen
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                         .allowedHeaders("*") // Permite todos los headers
                         .allowCredentials(true); // Permitir autenticación con cookies o headers
