453737c286162bd3be37aec721862ffd9c39d3c0
Juan Garcia Carballo <juangarciacarballo@gmail.com>
2025-03-11 22:18:50 +0100

    feat: add find all comanies only public information method

--- src/main/java/com/caronte/caronte/company/CompanyService.java
+++ src/main/java/com/caronte/caronte/company/CompanyService.java
@@ -1,10 +1,13 @@
 package com.caronte.caronte.company;
 
+import java.util.List;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.caronte.caronte.auth.payload.response.CompanyUpdateRequest;
+import com.caronte.caronte.plan.PlanType;
 
 
 
@@ -38,6 +41,22 @@ public class CompanyService {
         companyToUpdate.setDescription(request.getDescription());
         companyToUpdate.setPassword(this.passwordEncoder.encode(request.getPassword()));
         return companyRepository.save(companyToUpdate);
+    }
+
+    @Transactional
+    public List<CompanyDTO> findAllCompaniesPublicInformation(){
+        List<Company> companiesWithPlan = companyRepository.findByPlan_PlanType(PlanType.PREMIUM);
+        return companiesWithPlan.stream().map(company -> new CompanyDTO(
+            company.getName(), 
+            company.getEmail(), 
+            company.getTelephone(), 
+            company.getAddress(), 
+            company.getCity(), 
+            company.getZipCode(), 
+            company.getImageUrl(), 
+            company.getDescription(),
+            company.getNif()))
+            .toList();
     }
 
     
