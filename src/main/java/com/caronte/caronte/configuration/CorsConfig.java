d8d88b59d7516b3843aa6bf2140257cd10e52e79
jormunrod <jorgemr@protonmail.ch>
2025-03-16 13:53:14 +0100

    chore: update CORS configuration to allow origins for sprint 2

--- src/main/java/com/caronte/caronte/configuration/CorsConfig.java
+++ src/main/java/com/caronte/caronte/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://sprint1.caronte.site", "http://sprint1.caronte.site", "https://www.sprint1.caronte.site", "http://www.sprint1.caronte.site")
+                        .allowedOrigins("https://sprint2.caronte.site", "http://sprint2.caronte.site", "https://www.sprint2.caronte.site", "http://www.sprint2.caronte.site")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- src/main/java/com/caronte/caronte/configuration/SecurityConfig.java
+++ src/main/java/com/caronte/caronte/configuration/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("https://sprint1.caronte.site", "http://sprint1.caronte.site", "https://www.sprint1.caronte.site", "http://www.sprint1.caronte.site")); // ✅ Asegurar que el frontend tiene acceso
+        config.setAllowedOrigins(Arrays.asList("https://sprint2.caronte.site", "http://sprint2.caronte.site", "https://www.sprint2.caronte.site", "http://www.sprint2.caronte.site"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         source.registerCorsConfiguration("/**", config);
@@ -99,7 +99,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("https://sprint1.caronte.site", "http://sprint1.caronte.site", "https://www.sprint1.caronte.site", "http://www.sprint1.caronte.site"));
+        config.setAllowedOrigins(Arrays.asList("https://sprint2.caronte.site", "http://sprint2.caronte.site", "https://www.sprint2.caronte.site", "http://www.sprint2.caronte.site"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         source.registerCorsConfiguration("/**", config);
