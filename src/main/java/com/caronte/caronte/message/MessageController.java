b0936facf471579c09bb6c122d95890f225ef198
davguifer <davguifer@gmail.com>
2025-03-17 10:24:51 +0100

    feat: implement MessageController for creating messages

---
+++ src/main/java/com/caronte/caronte/message/MessageController.java
@@ -0,0 +1,41 @@
+package com.caronte.caronte.message;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.caronte.caronte.configuration.services.UserDetailsImpl;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/messages")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createMessage(@RequestBody @Valid CreateMessageRequestDto request, Authentication authentication) {
+        try {
+            UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+            Long customerId = userPrincipal.getId();
+            Message createdMessage = messageService.createMessage(request, customerId);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("messageId", createdMessage.getId()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", e.getMessage()));
+        }
+    }
+}
