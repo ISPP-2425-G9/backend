df0d8cb303c86a4278f36c41dce5ad1a26f42d9f
Juan Garcia Carballo <juangarciacarballo@gmail.com>
2025-03-19 23:44:33 +0100

    fix: add transactional to changePlan

--- src/main/java/com/caronte/caronte/plan/PlanService.java
+++ src/main/java/com/caronte/caronte/plan/PlanService.java
@@ -2,6 +2,7 @@ package com.caronte.caronte.plan;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
@@ -15,12 +16,12 @@ public class PlanService {
         this.planRepository = planRepository;
     }
 
+    @Transactional
     public Plan changePlan(Long id, PlanType planType){
         Optional<Plan> plan = planRepository.findById(id);
         if(plan.isPresent()){
             plan.get().setPlanType(planType);
             return planRepository.save(plan.get());
         } throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plan con ID " + id + " no encontrado");
-
     }
 }
