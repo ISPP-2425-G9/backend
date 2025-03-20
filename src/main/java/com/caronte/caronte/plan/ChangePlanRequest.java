ae96ed9964037da5bb553464dbe5fa10e555d25f
luchersol <lmherencia2003@gmail.com>
2025-03-20 18:17:09 +0100

    refactor: create utility methods to throw error and put validations in DTOs

--- src/main/java/com/caronte/caronte/plan/ChangePlanRequest.java
+++ src/main/java/com/caronte/caronte/plan/ChangePlanRequest.java
@@ -2,12 +2,15 @@ package com.caronte.caronte.plan;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ChangePlanRequest {
 
     private String paymentMethodId;
+
+    @NotNull
     private PlanType planType;
 
     @JsonIgnore
--- src/main/java/com/caronte/caronte/plan/PlanController.java
+++ src/main/java/com/caronte/caronte/plan/PlanController.java
@@ -1,15 +1,15 @@
 package com.caronte.caronte.plan;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.caronte.caronte.company.CompanyDTO;
+import com.caronte.caronte.configuration.services.StripeService;
+import com.caronte.caronte.user.User;
 import com.caronte.caronte.user.UserService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/plans")
@@ -17,30 +17,26 @@ public class PlanController {
 
     private PlanService planService;
     private UserService userService;
+    private StripeService stripeService;
 
-    public PlanController(PlanService planService, UserService userService) {
+    public PlanController(PlanService planService, UserService userService, StripeService stripeService) {
         this.planService = planService;
         this.userService = userService;
+        this.stripeService = stripeService;
     }
 
-    @PutMapping("/{userId}/premium")
-    public Plan setPremiumPlan(@PathVariable Long userId) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (userService.findCurrentUser().getId() != userId & !auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "No puedes cambiar el plan de otro usuario");
+    @PutMapping("/{userId}")
+    public ResponseEntity<?> changePlan(@PathVariable Long userId, @RequestBody ChangePlanRequest changePlanRequest) {
+        try {
+            User user = userService.checkIsCurrentUser(userId);
+            String subscriptionId = changePlanRequest.isPremium() ?
+                    stripeService.subscription(changePlanRequest.getPaymentMethodId(), user): null;
+            Plan plan = planService.changePlan(user, changePlanRequest.getPlanType(), subscriptionId);
+            return ResponseEntity.ok(plan);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
 
-        Long planId = userService.getPlanId(userId);
-        return planService.changePlan(planId, PlanType.PREMIUM);
     }
 
-    @PutMapping("/{userId}/free")
-    public Plan setFreePlan(@PathVariable Long userId) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (userService.findCurrentUser().getId() != userId & !auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "No puedes cambiar el plan de otro usuario");
-        }
-        Long planId = userService.getPlanId(userId);
-        return planService.changePlan(planId, PlanType.FREE);
-    }
 }
