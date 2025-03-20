bc48e471e21e2324bc5aa1b7912db32e54437664
luchersol <lmherencia2003@gmail.com>
2025-03-20 17:25:37 +0100

    feat: add stripe service to subscription

---
+++ src/main/java/com/caronte/caronte/configuration/services/StripeService.java
@@ -0,0 +1,67 @@
+package com.caronte.caronte.configuration.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.caronte.caronte.company.Company;
+import com.caronte.caronte.plan.Plan;
+import com.caronte.caronte.user.User;
+import com.caronte.caronte.user.UserRepository;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import com.stripe.model.Subscription;
+import com.stripe.param.CustomerCreateParams;
+import com.stripe.param.CustomerListParams;
+import com.stripe.param.SubscriptionCreateParams;
+
+@Service
+public class StripeService {
+
+    private final UserRepository userRepository;
+
+
+    @Value("${stripe.customer.premium.price.id}")
+    private String customerPremiumPriceId;
+
+    @Value("${stripe.company.premium.price.id}")
+    private String companyPremiumPriceId;
+
+
+    StripeService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Transactional
+    public void subscription(String paymentMethodId, User user) throws StripeException {
+        List<Customer> customers = Customer.list(CustomerListParams.builder()
+                .setEmail(user.getEmail())
+                .setLimit(1L)
+                .build()).getData();
+
+        // Este "Customer" es referido a la clase Customer de Stripe, no el de Caronte
+        Customer customer = !customers.isEmpty() ? customers.getFirst()
+                : Customer.create(
+                        CustomerCreateParams.builder()
+                                .setEmail(user.getEmail())
+                                .build());
+
+        String priceId = user instanceof Company ? companyPremiumPriceId : customerPremiumPriceId;
+        SubscriptionCreateParams params = SubscriptionCreateParams.builder()
+                .setCustomer(customer.getId())
+                .addItem(
+                        SubscriptionCreateParams.Item.builder()
+                                .setPrice(priceId)
+                                .build())
+                .setDefaultPaymentMethod(paymentMethodId)
+                .build();
+
+        Subscription subscription = Subscription.create(params);
+        Plan plan = Plan.newPlanPremium(subscription.getId());
+        user.setPlan(plan);
+        userRepository.save(user);
+    }
+}
