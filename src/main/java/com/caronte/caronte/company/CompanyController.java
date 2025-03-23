997a5f2e127b93c7c34a91395f5a765201f84b4c
Jaime-Linares <jailinbar@alum.us.es>
2025-03-23 13:51:43 +0100

    refactor: remove unused imports

--- src/main/java/com/caronte/caronte/company/CompanyController.java
+++ src/main/java/com/caronte/caronte/company/CompanyController.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/companies")
 public class CompanyController {
