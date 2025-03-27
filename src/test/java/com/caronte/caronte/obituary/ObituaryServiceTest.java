37b2cef82c8b847ac9ffbcbca44d870c38b52002
JavierRodriguez <javrodrei@alum.us.es>
2025-03-27 10:53:00 +0100

    test: add some ObituaryServiceTests

--- src/test/java/com/caronte/caronte/obituary/ObituaryServiceTest.java
+++ src/test/java/com/caronte/caronte/obituary/ObituaryServiceTest.java
@@ -12,9 +12,9 @@ import com.caronte.caronte.obituary.ObituraryRequestDto.ContactDto;
 import com.caronte.caronte.receiver.ReceiverService;
 import com.caronte.caronte.util.MediaHandler;
 import com.caronte.caronte.deathCertificate.DeathCertificate;
+import com.caronte.caronte.deathCertificate.DeathCertificateRequestDTO;
 import com.caronte.caronte.deathCertificate.DeathCertificateService;
 
-import java.awt.image.BufferedImage;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -53,6 +53,7 @@ public class ObituaryServiceTest {
     private Obituary obituary;
     private Obituary obituary2;
     private DeathCertificate certificate;
+    private List<ContactDto> contacts;
 
     @BeforeEach
     public void setUp() {
@@ -62,19 +63,31 @@ public class ObituaryServiceTest {
         customer.setId(1L);
         customer.setName("Alfonso Manuel Giraldillo");
         customer.setEmail("alfonso@example.com");
+        customer.setDni("12345678A");
+
+        contacts = new ArrayList<>();
+        ContactDto contact1 = new ContactDto();
+        contact1.setName("Contacto1Nombre");
+        contact1.setPhone("68556790743");
+        contact1.setEmail("email1@gmail.com");
+        contacts.add(contact1);
 
         imageTemplate = new ImageTemplate();
         imageTemplate.setId(1L);
 
         certificate = new DeathCertificate();
         certificate.setId(1L);
+        certificate.setUrl("https://example.com/certificate.pdf");
 
         requestDto = new ObituraryRequestDto();
-        requestDto.setName("John Doe");
-        requestDto.setFarewellMessage("Goodbye!");
-        requestDto.setFarewellPhrase("Rest in peace.");
-        requestDto.setIsMine(true);
+        requestDto.setName("Alfonso Manuel Giraldillo");
+        requestDto.setFarewellMessage("Esto es un mensaje de despedida");
+        requestDto.setFarewellPhrase("Esto es una frase de despedida");
         requestDto.setImageTemplate_id(1L);
+        requestDto.setIsMine(true);
+        requestDto.setBirthDate(null);
+        requestDto.setDeathDate(null);
+        requestDto.setContacts(contacts);
         requestDto.setCustomImage("https://static.nationalgeographicla.com/files/styles/image_3200/public/comedy-wildlife-awards-squirel-stop.jpg"); 
         
     
@@ -85,6 +98,7 @@ public class ObituaryServiceTest {
         obituary.setFarewellPhrase("Esto es una frase de despedida");
         obituary.setCustomImageUrl("uploaded-image-url");
         obituary.setBirthDate(null);
+        obituary.setWordColor("0,0,0");
         obituary.setDeathDate(null);
         obituary.setCustomer(customer);
         obituary.setImageTemplate(imageTemplate);
@@ -99,42 +113,24 @@ public class ObituaryServiceTest {
         obituary2.setFarewellPhrase("Esto es una frase de despedida");
         obituary2.setCustomImageUrl("uploaded-image-url");
         obituary2.setBirthDate(null);
-        obituary2.setDeathDate(null);
+        obituary2.setDeathDate(LocalDate.of(2023, 3, 1));
         obituary2.setCustomer(customer);
         obituary2.setImageTemplate(imageTemplate);
         obituary2.setDeathCertificate(null);
         obituary2.setIsMine(false);
 
+
+
     }
 @Test
     public void testCreateObituaryWithReceiversIsMineTrue() {
-        String base64Image = "https://static.nationalgeographicla.com/files/styles/image_3200/public/comedy-wildlife-awards-squirel-stop.jpg";
     
         when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
         when(imageTemplateService.findById(1L)).thenReturn(imageTemplate); 
-    
-        ObituraryRequestDto requestDto = new ObituraryRequestDto();
-        requestDto.setName("Alfonso Manuel Giraldillo");
-        requestDto.setFarewellMessage("Esto es un mensaje de despedida");
-        requestDto.setFarewellPhrase("Esto es una frase de despedida");
-        requestDto.setImageTemplate_id(1L);
-        requestDto.setIsMine(true);
-        requestDto.setCustomImage(base64Image);  
-        requestDto.setBirthDate(null);
-        requestDto.setDeathDate(null);
-
-        List<ContactDto> contacts = new ArrayList<>();
-        ContactDto contact1 = new ContactDto();
-        contact1.setName("Contacto1Nombre");
-        contact1.setPhone("68556790743");
-        contact1.setEmail("email1@gmail.com");
-        contacts.add(contact1);
-        requestDto.setContacts(contacts);
-        
+            
         when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary);
         Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
         when(receiverService.saveObituaryReceiver(eq("Contacto1Nombre"), eq("68556790743"), eq("email1@gmail.com"), eq(obituary_test))).thenReturn(null);
-        
 
         verify(customerRepository).findById(1L);
         verify(imageTemplateService).findById(1L);
@@ -153,6 +149,114 @@ public class ObituaryServiceTest {
             verify(receiverService).saveObituaryReceiver(eq(contact.getName()), eq(contact.getPhone()), eq(contact.getEmail()), eq(obituary_test));
         }
     }
+
+    @Test
+    public void testCreateObituaryWithReceiversIsMineFalse() {
+
+        when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
+        when(imageTemplateService.findById(1L)).thenReturn(imageTemplate);
+    
+        requestDto.setIsMine(false);
+        requestDto.setDeathDate(LocalDate.of(2023, 3, 1));
+
+        DeathCertificateRequestDTO deathCertificateRequestDTO = new DeathCertificateRequestDTO();
+        deathCertificateRequestDTO.setDni("12345678Z");
+        deathCertificateRequestDTO.setFile("https://example.com/certificate.pdf");
+        deathCertificateRequestDTO.setIsVerificate(true);
+
+        requestDto.setDeathCertificate(deathCertificateRequestDTO);
+
+        when(deathCertificateService.createDeathCertificateAndRelations(any(DeathCertificateRequestDTO.class), anyLong())).thenReturn(certificate);
+        when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary2);
+        Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
+        
+        verify(customerRepository).findById(1L);
+        verify(imageTemplateService).findById(1L);
+        verify(obituaryRepository, times(1)).save(any(Obituary.class));
+        
+        assertNotNull(obituary_test);
+        assertEquals("Alfonso Manuel Giraldillo", obituary_test.getName());
+        assertEquals("Esto es un mensaje de despedida", obituary_test.getFarewellMessage());
+        assertEquals("Esto es una frase de despedida", obituary_test.getFarewellPhrase());
+        assertEquals("uploaded-image-url", obituary_test.getCustomImageUrl());
+        assertNull(obituary_test.getBirthDate());
+        assertNotNull(obituary_test.getDeathDate());
+    }
+
+@Test
+public void testCreateObituaryWithDefaultWordColor() {
+    
+    when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
+    when(imageTemplateService.findById(1L)).thenReturn(imageTemplate);
+    
+    requestDto.setWordColor(null);  
+    requestDto.setContacts(contacts);
+    
+    when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary);
+    
+    Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
+    
+    verify(customerRepository).findById(1L);
+    verify(imageTemplateService).findById(1L);
+    verify(obituaryRepository, times(1)).save(any(Obituary.class));
+    
+
+    assertNotNull(obituary_test);
+    assertEquals("0,0,0", obituary_test.getWordColor());
+}
+
+@Test
+public void testCreateObituaryWithNoContacts() {
+    
+    when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
+    when(imageTemplateService.findById(1L)).thenReturn(imageTemplate);
+    requestDto.setContacts(new ArrayList<>());  
+    
+    when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary);
+    
+    Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
+
+    verify(customerRepository).findById(1L);
+    verify(imageTemplateService).findById(1L);
+    verify(obituaryRepository, times(1)).save(any(Obituary.class));
+    
+    assertNotNull(obituary_test);
+    assertTrue(requestDto.getContacts().isEmpty());  
+}
+
+@Test
+public void testCreateObituaryWithMultipleContacts() {
+
+    when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
+    when(imageTemplateService.findById(1L)).thenReturn(imageTemplate);
+
+    List<ObituraryRequestDto.ContactDto> contacts = new ArrayList<>();
+    ObituraryRequestDto.ContactDto contact1 = new ObituraryRequestDto.ContactDto();
+    contact1.setName("Contacto1");
+    contact1.setPhone("123456789");
+    contact1.setEmail("contact1@example.com");
+    contacts.add(contact1);
+    
+    ObituraryRequestDto.ContactDto contact2 = new ObituraryRequestDto.ContactDto();
+    contact2.setName("Contacto2");
+    contact2.setPhone("987654321");
+    contact2.setEmail("contact2@example.com");
+    contacts.add(contact2);
+    
+    requestDto.setContacts(contacts); 
+    
+    when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary);
+    when(receiverService.saveObituaryReceiver(anyString(), anyString(), anyString(), any(Obituary.class))).thenReturn(null);
+    
+    Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
+    
+    verify(customerRepository).findById(1L);
+    verify(imageTemplateService).findById(1L);
+    verify(obituaryRepository, times(1)).save(any(Obituary.class));
+    verify(receiverService, times(2)).saveObituaryReceiver(anyString(), anyString(), anyString(), any(Obituary.class)); 
+    assertNotNull(obituary_test);
+}
+
 
 
     @Test
@@ -248,8 +352,6 @@ public void testUpdateObituaryWithReceivers_Success() {
     assertEquals("https://example.com/nueva-imagen.jpg", updatedObituary.getCustomImageUrl());
     assertEquals("255,255,255", updatedObituary.getWordColor());
     assertEquals(imageTemplate, updatedObituary.getImageTemplate());
-
-    // Verificamos que se han llamado los métodos de receivers
     verify(receiverService).deleteReceiversByObituaryId(updatedObituary);
     verify(receiverService).saveObituaryReceiver("ContactoActualizado", "1234567890", "actualizado@example.com", updatedObituary);
 }
