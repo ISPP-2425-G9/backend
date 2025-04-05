c31b1eb679127dde888fa2906dc262b1e3fcebe9
JavierRodriguez <javrodrei@alum.us.es>
2025-04-05 14:05:59 +0200

    refactor: update MessageRequestDto to improve validation messages and remove unnecessary blank lines
    Co-authored-by: David Guillén davguifer@users.noreply.github.com

--- src/main/java/com/caronte/caronte/message/DTOs/MessageRequestDto.java
+++ src/main/java/com/caronte/caronte/message/DTOs/MessageRequestDto.java
@@ -1,9 +1,12 @@
 package com.caronte.caronte.message.DTOs;
 
 import java.util.List;
+import static com.caronte.caronte.util.RegexContants.REGEX_TELEPHONE;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,8 +17,10 @@ public class MessageRequestDto {
 
     private Long messageId = null;
 
+    @Size(max = 80, message = "The title message must be at most 80 characters long")
     private String title;
 
+    @Size(max = 2000, message = "The  message must be at most 2000 characters long")
     private String body;
 
     @Size(max = 5)
@@ -24,7 +29,6 @@ public class MessageRequestDto {
     @NotNull
     private Boolean isLastWill = false;
     
-
     private List<RecipientDto> recipients;
 
     public List<RecipientDto> getRecipients() {
@@ -34,10 +38,14 @@ public class MessageRequestDto {
     @Getter
     @Setter
     public static class RecipientDto {
+        @NotBlank(message = "The contact name cannot be empty")
         private String name;
-        @NotBlank
+
+        @Pattern(regexp = REGEX_TELEPHONE, message = "The phone number must contain 9 digits")
         private String telephone;
-        @NotBlank
+
+        @Email(message = "The email is not valid")
+        @NotBlank(message = "The email cannot be empty")
         private String email;
     }
 }
