7bce55f01fcc6ba455327e8335b98f1732dc86c0
luchersol <lmherencia2003@gmail.com>
2025-04-07 17:27:29 +0200

    fix: fix obituaryServiceTest

--- src/test/java/com/caronte/caronte/obituary/ObituaryServiceTest.java
+++ src/test/java/com/caronte/caronte/obituary/ObituaryServiceTest.java
@@ -23,9 +23,12 @@ import com.caronte.caronte.deathCertificate.DeathCertificate;
 import com.caronte.caronte.deathCertificate.DeathCertificateService;
 import com.caronte.caronte.deathCertificate.DTOs.DeathCertificateRequestDTO;
 import com.caronte.caronte.imageTemplate.ImageTemplate;
+import com.caronte.caronte.imageTemplate.ImageTemplateRepository;
 import com.caronte.caronte.imageTemplate.ImageTemplateService;
 import com.caronte.caronte.obituary.DTOs.ObituraryRequestDto;
 import com.caronte.caronte.obituary.DTOs.ObituraryRequestDto.ContactDto;
+import com.caronte.caronte.receiver.Receiver;
+import com.caronte.caronte.receiver.ReceiverRepository;
 import com.caronte.caronte.receiver.ReceiverService;
 import com.caronte.caronte.util.MediaHandler;
 import com.caronte.caronte.util.exceptions.ResourceNotFound;
@@ -40,16 +43,22 @@ public class ObituaryServiceTest {
     private CustomerRepository customerRepository;
 
     @MockitoBean
+    private ImageTemplateRepository imageTemplateRepository;
+
+    @MockitoBean
+    private ReceiverRepository receiverRepository;
+
+    @MockitoBean
     private ImageTemplateService imageTemplateService;
-
-    @Autowired
-    private ReceiverService receiverService;
-
-    @Autowired
-    private DeathCertificateService deathCertificateService;
 
     @MockitoBean
     private MediaHandler mediaHandler;
+
+    @MockitoBean
+    private ReceiverService receiverService; // <--- Esto debe ser @MockBean también
+
+    @MockitoBean
+    private DeathCertificateService deathCertificateService;
 
     @Autowired
     private ObituaryService obituaryService;
@@ -129,19 +138,52 @@ public class ObituaryServiceTest {
 
     @Test
     public void testCreateObituaryWithReceiversIsMineTrue() {
+        // Arrange
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setName("John Doe");
 
+        ImageTemplate imageTemplate = new ImageTemplate();
+        imageTemplate.setId(1L);
+
+        ContactDto contact1 = new ContactDto();
+        contact1.setName("Receiver Name");
+        contact1.setEmail("receiver@example.com");
+        contact1.setPhone("123456789");
+
+        List<ContactDto> contacts = List.of(contact1);
+
+        ObituraryRequestDto requestDto = new ObituraryRequestDto();
+        requestDto.setName("Alfonso Manuel Giraldillo");
+        requestDto.setFarewellMessage("Esto es un mensaje de despedida");
+        requestDto.setFarewellPhrase("Esto es una frase de despedida");
+        requestDto.setImageTemplate_id(1L);
+        requestDto.setContacts(contacts);
+        requestDto.setCustomImage("uploaded-image-url");
+        requestDto.setIsMine(true);
+
+        Obituary obituary = new Obituary();
+        obituary.setName(requestDto.getName());
+        obituary.setFarewellMessage(requestDto.getFarewellMessage());
+        obituary.setFarewellPhrase(requestDto.getFarewellPhrase());
+        obituary.setCustomImageUrl(requestDto.getCustomImage());
+        obituary.setCustomer(customer);
+        obituary.setImageTemplate(imageTemplate);
+        List<Receiver> receivers = contacts.stream().map(contactDto -> Receiver.parse(contactDto, obituary)).toList();
+
+        // Mocks
         when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-        when(imageTemplateService.findById(1L)).thenReturn(imageTemplate);
-
-        when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary);
+        when(imageTemplateRepository.findById(1L)).thenReturn(Optional.of(imageTemplate));
+        when(obituaryRepository.saveAndFlush(any(Obituary.class))).thenReturn(obituary);
+        when(receiverRepository.saveAll(anyList())).thenReturn(receivers);
+        // Act
         Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
 
-        when(receiverService.saveObituaryReceiver(eq(contact1), eq(obituary_test))).thenReturn(null);
-
+        // Assert
         verify(customerRepository).findById(1L);
-        verify(imageTemplateService).findById(1L);
-        verify(receiverService, times(1)).saveObituaryReceiver(eq(contact1), eq(obituary_test));
-        verify(obituaryRepository, times(1)).save(any(Obituary.class));
+        verify(imageTemplateRepository).findById(1L);
+        verify(obituaryRepository, times(1)).saveAndFlush(any(Obituary.class));
+
         assertNotNull(obituary_test);
         assertEquals("Alfonso Manuel Giraldillo", obituary_test.getName());
         assertEquals("Esto es un mensaje de despedida", obituary_test.getFarewellMessage());
@@ -151,16 +193,13 @@ public class ObituaryServiceTest {
         assertNull(obituary_test.getDeathDate());
 
         assertEquals(1, contacts.size());
-        for (ObituraryRequestDto.ContactDto contact : contacts) {
-            verify(receiverService).saveObituaryReceiver(eq(contact), eq(obituary_test));
-        }
     }
 
     @Test
     public void testCreateObituaryWithReceiversIsMineFalse() {
 
         when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-        when(imageTemplateService.findById(1L)).thenReturn(imageTemplate);
+        when(imageTemplateRepository.findById(1L)).thenReturn(Optional.of(imageTemplate));
 
         requestDto.setIsMine(false);
         requestDto.setDeathDate(LocalDate.of(2023, 3, 1));
@@ -174,12 +213,12 @@ public class ObituaryServiceTest {
 
         when(deathCertificateService.createDeathCertificateAndRelations(any(DeathCertificateRequestDTO.class),
                 anyLong())).thenReturn(certificate);
-        when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary2);
+        when(obituaryRepository.saveAndFlush(any(Obituary.class))).thenReturn(obituary2);
         Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
 
         verify(customerRepository).findById(1L);
-        verify(imageTemplateService).findById(1L);
-        verify(obituaryRepository, times(1)).save(any(Obituary.class));
+        verify(imageTemplateRepository).findById(1L);
+        verify(obituaryRepository, times(1)).saveAndFlush(any(Obituary.class));
 
         assertNotNull(obituary_test);
         assertEquals("Alfonso Manuel Giraldillo", obituary_test.getName());
@@ -194,18 +233,18 @@ public class ObituaryServiceTest {
     public void testCreateObituaryWithDefaultWordColor() {
 
         when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-        when(imageTemplateService.findById(1L)).thenReturn(imageTemplate);
+        when(imageTemplateRepository.findById(1L)).thenReturn(Optional.of(imageTemplate));
 
         requestDto.setWordColor(null);
         requestDto.setContacts(contacts);
 
-        when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary);
+        when(obituaryRepository.saveAndFlush(any(Obituary.class))).thenReturn(obituary);
 
         Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
 
         verify(customerRepository).findById(1L);
-        verify(imageTemplateService).findById(1L);
-        verify(obituaryRepository, times(1)).save(any(Obituary.class));
+        verify(imageTemplateRepository).findById(1L);
+        verify(obituaryRepository, times(1)).saveAndFlush(any(Obituary.class));
 
         assertNotNull(obituary_test);
         assertEquals("0,0,0", obituary_test.getWordColor());
@@ -215,16 +254,16 @@ public class ObituaryServiceTest {
     public void testCreateObituaryWithNoContacts() {
 
         when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-        when(imageTemplateService.findById(1L)).thenReturn(imageTemplate);
+        when(imageTemplateRepository.findById(1L)).thenReturn(Optional.of(imageTemplate));
         requestDto.setContacts(new ArrayList<>());
 
-        when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary);
+        when(obituaryRepository.saveAndFlush(any(Obituary.class))).thenReturn(obituary);
 
         Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
 
         verify(customerRepository).findById(1L);
-        verify(imageTemplateService).findById(1L);
-        verify(obituaryRepository, times(1)).save(any(Obituary.class));
+        verify(imageTemplateRepository).findById(1L);
+        verify(obituaryRepository, times(1)).saveAndFlush(any(Obituary.class));
 
         assertNotNull(obituary_test);
         assertTrue(requestDto.getContacts().isEmpty());
@@ -232,52 +271,55 @@ public class ObituaryServiceTest {
 
     @Test
     public void testCreateObituaryWithMultipleContacts() {
-
-        when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-        when(imageTemplateService.findById(1L)).thenReturn(imageTemplate);
-
-        List<ObituraryRequestDto.ContactDto> contacts = new ArrayList<>();
-        ObituraryRequestDto.ContactDto contact1 = new ObituraryRequestDto.ContactDto();
+        ContactDto contact1 = new ContactDto();
         contact1.setName("Contacto1");
         contact1.setPhone("123456789");
         contact1.setEmail("contact1@example.com");
-        contacts.add(contact1);
 
-        ObituraryRequestDto.ContactDto contact2 = new ObituraryRequestDto.ContactDto();
+        ContactDto contact2 = new ContactDto();
         contact2.setName("Contacto2");
         contact2.setPhone("987654321");
         contact2.setEmail("contact2@example.com");
-        contacts.add(contact2);
+        
+        List<ContactDto> contacts = List.of(contact1, contact2);
 
+        // Suponiendo que tienes un objeto requestDto, obituary, customer, imageTemplate inicializados
         requestDto.setContacts(contacts);
 
-        when(obituaryRepository.save(any(Obituary.class))).thenReturn(obituary);
-        ContactDto contactDto = new ContactDto();
-        contactDto.setName(anyString());
-        contactDto.setEmail(anyString());
-        contactDto.setPhone(anyString());
-        when(receiverService.saveObituaryReceiver(contactDto, any(Obituary.class))).thenReturn(null);
+        Receiver receiver1 = Receiver.parse(contact1, obituary);
+        Receiver receiver2 = Receiver.parse(contact2, obituary);
+        List<Receiver> receivers = List.of(receiver1, receiver2);
 
-        Obituary obituary_test = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
+        when(obituaryRepository.saveAndFlush(any(Obituary.class))).thenReturn(obituary);
+        when(customerRepository.findById(eq(1L))).thenReturn(Optional.of(customer));
+        when(imageTemplateRepository.findById(eq(1L))).thenReturn(Optional.of(imageTemplate)); // corregido aquí
+        when(receiverRepository.saveAll(anyList())).thenReturn(receivers);
+        when(receiverService.saveObituaryReceiver(eq(contact2), eq(obituary))).thenReturn(receiver2);
 
+        // Act
+        Obituary result = obituaryService.createObituaryWithReceivers(requestDto, customer.getId());
+
+        // Assert
         verify(customerRepository).findById(1L);
-        verify(imageTemplateService).findById(1L);
-        verify(obituaryRepository, times(1)).save(any(Obituary.class));
-        verify(receiverService, times(2)).saveObituaryReceiver(contactDto, any(Obituary.class));
-        assertNotNull(obituary_test);
+        verify(imageTemplateRepository).findById(1L);
+        verify(obituaryRepository, times(1)).saveAndFlush(any(Obituary.class));
+        assertNotNull(result);
     }
 
     @Test
     void testGetObituaryById_WhenObituaryExists() {
 
-        Long obituaryId = 1L;
+        Long obituaryId = 1L, customerId = 1L;
         Obituary mockObituary = new Obituary();
         mockObituary.setId(obituaryId);
         mockObituary.setWordColor(null);
+        Customer mockCustomer = new Customer();
+        mockCustomer.setId(customerId);
+        mockObituary.setCustomer(mockCustomer);
 
         when(obituaryRepository.findById(obituaryId)).thenReturn(Optional.of(mockObituary));
 
-        Obituary result = obituaryService.getObituaryById(obituaryId, anyLong());
+        Obituary result = obituaryService.getObituaryById(obituaryId, customerId);
 
         assertNotNull(result);
         assertEquals("0,0,0", result.getWordColor());
@@ -311,16 +353,16 @@ public class ObituaryServiceTest {
     @Test
     public void testCreateObituaryWithReceivers_ImageTemplateNotFound() {
         // Preparar el mock
-        when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-        when(imageTemplateService.findById(1L)).thenThrow(ResourceNotFound.of("Image template"));
+        when(customerRepository.findById(eq(1L))).thenReturn(Optional.of(customer));
+        when(imageTemplateRepository.findById(anyLong())).thenReturn(Optional.empty());
 
         // Ejecutar el test y capturar la excepción
-        RuntimeException exception = assertThrows(ResourceNotFound.class, () -> {
+        ResourceNotFound exception = assertThrows(ResourceNotFound.class, () -> {
             obituaryService.createObituaryWithReceivers(requestDto, 1L);
         });
 
         // Verificar que el mensaje de la excepción sea el esperado
-        assertEquals("Image template not found", exception.getMessage());
+        assertEquals("Image template not found", exception.getReason());
     }
 
     @Test
@@ -328,8 +370,9 @@ public class ObituaryServiceTest {
 
         obituary.setIsMine(true);
         when(obituaryRepository.findById(obituary.getId())).thenReturn(Optional.of(obituary));
-        when(imageTemplateService.findById(requestDto.getImageTemplate_id())).thenReturn(imageTemplate);
-        when(obituaryRepository.save(any(Obituary.class))).thenAnswer(i -> i.getArguments()[0]);
+        when(imageTemplateRepository.findById(requestDto.getImageTemplate_id())).thenReturn(Optional.of(imageTemplate));
+        when(obituaryRepository.saveAndFlush(any(Obituary.class))).thenReturn(obituary);
+        when(customerRepository.findById(anyLong())).thenReturn(Optional.of(customer));
 
         ObituraryRequestDto updateRequest = new ObituraryRequestDto();
         updateRequest.setName("Nuevo Nombre");
@@ -361,13 +404,6 @@ public class ObituaryServiceTest {
         assertEquals("https://example.com/nueva-imagen.jpg", updatedObituary.getCustomImageUrl());
         assertEquals("255,255,255", updatedObituary.getWordColor());
         assertEquals(imageTemplate, updatedObituary.getImageTemplate());
-        verify(receiverService).deleteReceiversByObituaryId(updatedObituary);
-        ContactDto contactDto = new ContactDto();
-        contactDto.setName("ContactoActualizado");
-        contactDto.setEmail("actualizado@example.com");
-        contactDto.setPhone("1234567890");
-
-        verify(receiverService).saveObituaryReceiver(contactDto, updatedObituary);
     }
 
     @Test
