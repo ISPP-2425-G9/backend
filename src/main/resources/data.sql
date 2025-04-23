37681dfc45a9b7b8c942a09b6b4a6568c1cfdacc
JavierRodriguez <javrodrei@alum.us.es>
2025-04-23 17:50:44 +0200

    fix: correct customer_id associations in obituary and message inserts for accuracy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -133,15 +133,15 @@ INSERT INTO image_template (id , image_url) VALUES (1 ,'https://res.cloudinary.c
  (3, 'https://res.cloudinary.com/ds02duuid/image/upload/v1741707864/images/obituaries/templates/pl6ual71zcpcsfiv8n3q.jpg');
 
 INSERT INTO death_certificate(id, url, is_verified, dni ) VALUES (1, 'https://static.nationalgeographicla.com/files/styles/image_3200/public/comedy-wildlife-awards-squirel-stop.jpg',0, '26745987T'),
- (2, 'https://static.nationalgeographicla.com/files/styles/image_3200/public/comedy-wildlife-awards-squirel-stop.jpg',0, '267654238H'); 
+ (2, 'https://static.nationalgeographicla.com/files/styles/image_3200/public/comedy-wildlife-awards-squirel-stop.jpg',0, '26745987T'); 
 INSERT INTO obituary (id, name, birth_date, death_date, custom_image_url, farewell_message, farewell_phrase, is_mine, customer_id, image_template_id, death_certificate_id, word_color) VALUES 
 (1, 'Nombre Ejemplo 1', '1980-01-01', '2023-01-01', 'https://static.nationalgeographicla.com/files/styles/image_3200/public/comedy-wildlife-awards-squirel-stop.jpg', 'Mensaje de despedida 1', 'Frase de despedida 1', 1, 5, 1, 1 , NULL),
-(2, 'Nombre Ejemplo 2', '1975-05-15', '2022-12-20', 'https://static.nationalgeographicla.com/files/styles/image_3200/public/comedy-wildlife-awards-squirel-stop.jpg', 'Mensaje de despedida 2', 'Frase de despedida 2', 0, 6, 1, 1, NULL);
+(2, 'Nombre Ejemplo 2', '1975-05-15', '2022-12-20', 'https://static.nationalgeographicla.com/files/styles/image_3200/public/comedy-wildlife-awards-squirel-stop.jpg', 'Mensaje de despedida 2', 'Frase de despedida 2', 0, 6, 1, 2, NULL);
 
 INSERT INTO message (id ,body, code, is_last_will, title, customer_id, death_certificate_id ) VALUES 
 (1, 'Este es el último mensaje de prueba.', 'f/y2K0Ea56RQbdLqmUiLCw==', 0, 'Mensaje General', 5,1),
 (2, 'Otro mensaje de prueba.', 'f/y2K0Ea56RQbdLqmUiLCw==', 0, 'Mensaje General', 5,1),
-(3, 'Otro mensaje de prueba.', 'f/y2K0Ea56RQbdLqmUiLCw==', 0, 'Mensaje General', 6,2);
+(3, 'Otro mensaje de prueba.', 'f/y2K0Ea56RQbdLqmUiLCw==', 0, 'Mensaje General', 5,2);
 
 INSERT INTO image (id, image_url, message_id) VALUES 
 (1, 'https://static.nationalgeographicla.com/files/styles/image_3200/public/comedy-wildlife-awards-squirel-stop.jpg', 1),
