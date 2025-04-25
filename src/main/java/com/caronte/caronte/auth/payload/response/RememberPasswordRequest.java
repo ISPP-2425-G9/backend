70bef7558cd206a32eafb517f963a687ad8a0aad
luchersol <lmherencia2003@gmail.com>
2025-04-25 14:18:50 +0200

    fix: add new validation in password

--- src/main/java/com/caronte/caronte/auth/payload/response/RememberPasswordRequest.java
+++ src/main/java/com/caronte/caronte/auth/payload/response/RememberPasswordRequest.java
@@ -1,9 +1,12 @@
 package com.caronte.caronte.auth.payload.response;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.validation.constraints.NotBlank;
 
 public record RememberPasswordRequest(
     @NotBlank(message = "El email no puede estar en blanco") String email, 
-    @NotBlank(message = "La contraseña no puede estar en blanco") String password,
+    @NotBlank(message = "La contraseña no puede estar en blanco") 
+    @Length(min = 6, message = "La contraseña debe tener mínimo 6 caractéres") String password,
     @NotBlank(message = "Debes de introducir el código de verificación") String code) {
 }
