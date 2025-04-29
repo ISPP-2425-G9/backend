e728e4150be2f6190a445f664fc166778e7bd789
JavierRodriguez <javrodrei@alum.us.es>
2025-04-29 12:43:07 +0200

    fix: remove uniqueness constraint from dni field in Customer entity

--- src/main/java/com/caronte/caronte/customer/Customer.java
+++ src/main/java/com/caronte/caronte/customer/Customer.java
@@ -19,7 +19,7 @@ public class Customer extends User {
     @Column(nullable = false)
     private Boolean isActive;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String dni;
 
     @JsonIgnore
