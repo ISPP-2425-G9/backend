7c6c5d22810b8bf1c1b4ed781bf3072ca615d8f6
JavierRodriguez <javrodrei@alum.us.es>
2025-04-29 12:43:31 +0200

    fix: remove uniqueness constraint from email field in User entity

--- src/main/java/com/caronte/caronte/user/User.java
+++ src/main/java/com/caronte/caronte/user/User.java
@@ -26,7 +26,7 @@ public class User extends BaseEntity{
     @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
