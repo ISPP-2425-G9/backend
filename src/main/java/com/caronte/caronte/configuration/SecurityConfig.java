15f28c7c01cfa95080a34d8c4febfa79158cc26b
jormunrod <jorgemr@protonmail.ch>
2025-05-05 17:48:22 +0200

    fix: update CORS allowed origins in SecurityConfig to use wpl.caronte.site

--- src/main/java/com/caronte/caronte/configuration/SecurityConfig.java
+++ src/main/java/com/caronte/caronte/configuration/SecurityConfig.java
@@ -120,8 +120,8 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("https://ppl.caronte.site", "http://ppl.caronte.site",
-				"https://www.ppl.caronte.site", "http://www.ppl.caronte.site")); // ✅ Permitir solo el frontend
+        config.setAllowedOrigins(Arrays.asList("https://wpl.caronte.site", "http://wpl.caronte.site",
+				"https://www.wpl.caronte.site", "http://www.wpl.caronte.site")); // ✅ Permitir solo el frontend
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // ✅ Importante para el JWT
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         source.registerCorsConfiguration("/**", config);
